c5351e3afbddc5e9b3293d853ce65e37ede22521
MadhuMPandurangi <madhumpandurangi@gmail.com>
2023-04-10 17:22:51 +0530

    created separate thread to monitor disk utilization
    Signed-off-by: MadhuMPandurangi <madhumpandurangi@gmail.com>

--- script.py
+++ script.py
@@ -24,6 +24,8 @@ def compress():
 	print(sys.argv[1])
 	os.system(sys.argv[1])
 	final_time = time.time()
+	# print(start_time)
+	# print(final_time)
 	compression_time = round(final_time - start_time,2)
 	flag = 0
 	
@@ -34,41 +36,36 @@ def monitor():
 		cpu_usage_list.append(x)
 		y = psutil.virtual_memory()[2]
 		memory_percent_usage_list.append(y)
-		output = subprocess.check_output(["sar", "-d", "1", "1"])
-		output = output.decode("utf-8")
-		lines = output.split("\n")
-		for line in lines:
-			if re.search("^Average.*dev8.*", line):
-				fields = line.split()
-				rkbps = fields[3]
-				wkbps = fields[4]
-				dkbps = fields[5]
-				util = fields[9]
-				rkbps_list.append(float(rkbps))
-				wkbps_list.append(float(wkbps))
-				dkbps_list.append(float(dkbps))
-				util_list.append(float(util))
+		
 
-def pad_values():
-	global cpu_usage_list,memory_percent_usage_list
-	start_time = time.time()
-	while(time.time() - start_time <= 1.0):
-		x = psutil.cpu_percent(0.5)
-		y = psutil.virtual_memory()[2]
-		cpu_usage_list.append(x)
-		memory_percent_usage_list.append(y)
-	
+def diskMonitor():
+	global rkbps_list, wkbps_list, dkbps_list, util_list
+	output = subprocess.check_output(["sar", "-d", "1", "1"])
+	output = output.decode("utf-8")
+	lines = output.split("\n")
+	for line in lines:
+		if re.search("^Average.*dev8.*", line):
+			fields = line.split()
+			rkbps = fields[3]
+			wkbps = fields[4]
+			dkbps = fields[5]
+			util = fields[9]
+			rkbps_list.append(float(rkbps))
+			wkbps_list.append(float(wkbps))
+			dkbps_list.append(float(dkbps))
+			util_list.append(float(util))
 	
 if __name__ == "__main__":
 	
 	t1 = threading.Thread(target=compress,name="t1")
 	t2 = threading.Thread(target=monitor,name="t2")
+	t3 = threading.Thread(target=diskMonitor,name='t3')
 	
-	#pad_values()
-	
+	t3.start()
 	t2.start()
 	t1.start()
 	
+	t3.join()
 	t2.join()
 	t1.join()
 
@@ -81,8 +78,7 @@ if __name__ == "__main__":
 	if not os.path.exists(plotsPath):
 		os.makedirs(plotsPath)
 
-	ax = plt.gca()
-
+	
 	ls = sys.argv[2].split()
 	dir = ls[0]
 	#create directory for memory usage compression/decompression statistics
@@ -97,20 +93,19 @@ if __name__ == "__main__":
 	if not os.path.exists(cpu):
 		os.makedirs(cpu)
 
-
-	#pad_values()
-	
 	l = len(cpu_usage_list)
 	time_values = []
 	for i in range(l):
 		time_values.append(f"{(i)*0.5}-{(i+1)*0.5}")
 	
+	for i in cpu_usage_list:
+		print(i)
+
 	plt.plot(time_values, cpu_usage_list)	
 	plt.xlabel("Time interval")
 	plt.ylabel("CPU usage in percentage")
 	plt.title("CPU usage for " + sys.argv[2] + ', ' + sys.argv[3])
-	plt.tick_params(axis='x', labelrotation=90, labelsize=6, which='major', pad=8)
-	plt.xlim(0,10)
+	plt.tick_params(axis='x', labelrotation=-45)
 	plt.figtext(0.7,0.8,"Time taken : " + str(compression_time))
 	plt.savefig('./'+cpu+'/'+'/CPU usage for ' + sys.argv[2] + '.png')
 	plt.close()
@@ -119,8 +114,7 @@ if __name__ == "__main__":
 	plt.xlabel("Time interval")
 	plt.ylabel("Memory usage in percentage")
 	plt.title("Memory usage for " + sys.argv[2]+ ', ' + sys.argv[3])
-	plt.tick_params(axis='x', labelrotation=90, labelsize=6, which='major', pad=8)
-	plt.xlim(0,10)
+	plt.tick_params(axis='x', labelrotation=-45)
 	plt.figtext(0.7,0.8,"Time taken : " + str(compression_time))
 	plt.savefig('./'+mem+'/Memory usage for ' + sys.argv[2] + '.png')
 	plt.close()
